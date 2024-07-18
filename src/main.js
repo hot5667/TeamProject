9303a13b45f915df47cf731f9b62bdd4c849aff1
KimDoHyun1011 <hot99009@gmail.com>
2024-07-18 14:44:20 +0900

    모든 파일 삭제

--- src/main.js
+++
@@ -1,8 +0,0 @@
-export function initializeToggleDarkMode() {
-    const toggleButton = document.getElementById('toggle-button');
-    const toggleStatus = document.getElementById('toggle-status');
-
-    toggleButton.addEventListener('change', () => {
-        document.body.classList.toggle('dark-mode');
-    });
-}
