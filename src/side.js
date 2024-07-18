fa6c14cbcd3860e6d83bc292812c2fbe0e01a444
KimDoHyun1011 <hot99009@gmail.com>
2024-07-18 20:56:38 +0900

    수정 파일

--- src/side.js
+++ src/side.js
@@ -10,66 +10,104 @@ sidebarItems.forEach(item => {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-  const sidebarItems = document.querySelectorAll('.sidebar-item');
-  const subContainer = document.getElementById('Subcontainer');
-  const sectionsData = {
-      'KimDohyun-section': {
-          name: '김도현',
-          imgSrc: '/profile/김도현 사진.jpeg',
-          introText: '안녕하세요, 김도현입니다.'
-      },
-      'KimByungYeop-section': {
-          name: '김병엽',
-          imgSrc: '/profile/김병엽님 사진.jpeg',
-          introText: '안녕하세요, 김병엽입니다.'
-      },
-      'ChoHeeJin-section': {
-          name: '조희진',
-          imgSrc: '/profile/조희진님 사진.jpeg',
-          introText: '안녕하세요, 조희진입니다.'
-      },
-      'HanSuBin-section': {
-          name: '한수빈',
-          imgSrc: '/profile/한수빈님 사진.png',
-          introText: '안녕하세요, 한수빈입니다.'
-      },
-      'JanSungHyun-section': {
-          name: '장성현',
-          imgSrc: '/profile/정성현님 사진.jpeg',
-          introText: '안녕하세요, 장성현입니다.'
-      }
-  };
-
-  sidebarItems.forEach(item => {
-      item.addEventListener('click', function(event) {
-          event.preventDefault();
-          sidebarItems.forEach(item => {
-              item.classList.remove('clicked');
-          });
-          this.classList.add('clicked');
-          
-          const targetId = this.getAttribute('href').substring(1) + '-section';
-          subContainer.innerHTML = '';
-
-          renderTeamInfo(subContainer, sectionsData[targetId]);
-      });
+    const sidebarItems = document.querySelectorAll('.sidebar-item');
+    const subContainer = document.getElementById('Subcontainer');
+    const sectionsData = {
+        'KimDohyun-section': {
+            name: '김도현',
+            imgSrc: '/profile/김도현 사진.jpeg',
+            introText: '안녕하세요,김도현입니다.'
+        },
+        'KimByungYeop-section': {
+            name: '김병엽',
+            imgSrc: '/profile/김병엽님 사진.jpeg',
+            introText: '안녕하세요, 김병엽입니다.'
+        },
+        'ChoHeeJin-section': {
+            name: '조희진',
+            imgSrc: '/profile/조희진님 사진.jpeg',
+            introText: '안녕하세요, 조희진입니다.'
+        },
+        'HanSuBin-section': {
+            name: '한수빈',
+            imgSrc: '/profile/한수빈님 사진.png',
+            introText: '안녕하세요, 한수빈입니다.'
+        },
+        'JanSungHyun-section': {
+            name: '장성현',
+            imgSrc: '/profile/정성현님 사진.jpeg',
+            introText: '안녕하세요, 장성현입니다.'
+        }
+    };
+  
+    sidebarItems.forEach(item => {
+        item.addEventListener('click', function(event) {
+            event.preventDefault();
+            sidebarItems.forEach(item => {
+                item.classList.remove('clicked');
+            });
+            this.classList.add('clicked');
+            
+            const targetId = this.getAttribute('href').substring(1) + '-section';
+            subContainer.innerHTML = '';
+  
+            renderTeamInfo(subContainer, sectionsData[targetId], targetId);
+        });
+    });
+  
+    function renderTeamInfo(container, data, targetId) {
+        const teamDiv = document.createElement('div');
+        teamDiv.classList.add('Team1');
+        teamDiv.id = data.name;
+  
+        const img = document.createElement('img');
+        img.src = data.imgSrc;
+        img.alt = `${data.name} 프로필 사진`;
+  
+        const text = document.createElement('p');
+        text.classList.add('team-text');
+        
+        if (targetId === 'KimByungYeop-section') {
+            text.innerHTML = `
+            <div class = "KimByungYeop-text">
+                MBTI: ENTJ<br> 
+                취미: 운동, 코딩 영화: (역사적 배경을 담은) 국제시장, 남산의 부장들, 서울의 봄, 국가부도의 날, 1987<br> 
+                노래: (팝송) One Call Away(Charlie Puth), Off My  Face(Justin Bieber), Thinking out Loud(Ed Sheeran) Purpose(Etham)<br>
+                게임: (주로 여자친구랑) 오버워치, 크레이지 아케이드, 카트라이더'
+            </div>
+            `;
+        } else if (targetId === "ChoHeeJin-section"){
+            text.innerHTML = `
+            <div class = "ChoHeeJin-text">
+                MBTI: ENTJ<br> 
+                취미: 운동, 코딩 영화: (역사적 배경을 담은) 국제시장, 남산의 부장들, 서울의 봄, 국가부도의 날, 1987<br> 
+                노래: (팝송) One Call Away(Charlie Puth), Off My  Face(Justin Bieber), Thinking out Loud(Ed Sheeran) Purpose(Etham)<br>
+                게임: (주로 여자친구랑) 오버워치, 크레이지 아케이드, 카트라이더'
+            </div>
+            `;
+        } else if(targetId === "HanSuBin-section") {
+            text.innerHTML = `
+            <div class = "HanSuBin-text">
+                MBTI: ENTJ<br> 
+                취미: 운동, 코딩 영화: (역사적 배경을 담은) 국제시장, 남산의 부장들, 서울의 봄, 국가부도의 날, 1987<br> 
+                노래: (팝송) One Call Away(Charlie Puth), Off My  Face(Justin Bieber), Thinking out Loud(Ed Sheeran) Purpose(Etham)<br>
+                게임: (주로 여자친구랑) 오버워치, 크레이지 아케이드, 카트라이더'
+            </div>
+            `;
+        } else if (targetId === "JanSungHyun-section") {
+            text.innerHTML = `
+            <div class = "JanSungHyun-text">
+                MBTI: ENTJ<br> 
+                취미: 운동, 코딩 영화: (역사적 배경을 담은) 국제시장, 남산의 부장들, 서울의 봄, 국가부도의 날, 1987<br> 
+                노래: (팝송) One Call Away(Charlie Puth), Off My  Face(Justin Bieber), Thinking out Loud(Ed Sheeran) Purpose(Etham)<br>
+                게임: (주로 여자친구랑) 오버워치, 크레이지 아케이드, 카트라이더'
+            </div>
+            `;
+        }
+  
+        teamDiv.appendChild(img);
+        teamDiv.appendChild(text);
+        container.appendChild(teamDiv);
+    }
   });
-
-  function renderTeamInfo(container, data) {
-      const teamDiv = document.createElement('div');
-      teamDiv.classList.add('Team1');
-      teamDiv.id = data.name;
-
-      const img = document.createElement('img');
-      img.src = data.imgSrc;
-      img.alt = `${data.name} 프로필 사진`;
-
-      const text = document.createElement('p');
-      text.classList.add('team-text');
-      text.textContent = data.introText;
-
-      teamDiv.appendChild(img);
-      teamDiv.appendChild(text);
-      container.appendChild(teamDiv);
-  }
-});
+  
--- src/todolist.js
+++ src/todolist.js
@@ -1,49 +1,26 @@
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
-// import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
-// import { collection, addDoc } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
-// import { getDocs } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
-
-// const firebaseConfig = {
-//     apiKey: "AIzaSyD0o93MUJorSwMSWkVxu_bgzv_s3OAbyY0",
-//     authDomain: "teamshowcase-531d0.firebaseapp.com",
-//     projectId: "teamshowcase-531d0",
-//     storageBucket: "teamshowcase-531d0.appspot.com",
-//     messagingSenderId: "877618284112",
-//     appId: "1:877618284112:web:3881426e83a786e4a8f355",
-//     measurementId: "G-QXPFWCHZ70"   
-// };
-  
-// const app = initializeApp(firebaseConfig);
-// const db = getFirestore(app);
-  
 document.addEventListener("DOMContentLoaded", function () {
     const listbtn = document.getElementById('listbtn');
     const box = document.getElementById('box');
-
+    const badge = document.getElementById('badge');
+    let cloneCount = 0;
     listbtn.addEventListener('click', function() {
         box.classList.toggle('hide');
+        updateBageV();
     });
-
     const form = document.getElementById('form');
     const date = document.getElementById('date');
     const text = document.getElementById('text');
     const bottom = document.getElementById('bottom');
     const template = document.getElementById('template');
-
-    let cloneCount = 0;
-
     form.addEventListener("submit", function (event) {
         event.preventDefault();
-
         const textValue = text.value.trim();
         const dateValue = date.value;
-
+        updateBage();
         if (dateValue !== "" && textValue !== "" && cloneCount < 4) {
             const clone = template.content.cloneNode(true);
             const textDateElement = clone.querySelector('.text-date');
-
             textDateElement.textContent = `${textValue} (${dateValue})`;
-            
             const checkbox = clone.querySelector('input[type="checkbox"]');
             checkbox.addEventListener('change', function() {
                 if (checkbox.checked) {
@@ -52,17 +29,29 @@ document.addEventListener("DOMContentLoaded", function () {
                     textDateElement.classList.remove('strikethrough');
                 }
             });
-
             const deletebtn = clone.querySelector('.delete');
             deletebtn.addEventListener("click", function () {
                 deletebtn.parentNode.parentNode.remove();
                 cloneCount--;
+                updateBage();
             });
-
             bottom.appendChild(clone);
             cloneCount++;
             text.value = "";
             date.value = "";
+            updateBage();
         }
     });
+    function updateBage() {
+        badge.textContent = cloneCount;
+        updateBageV();
+    }
+    function updateBageV() {
+        if(cloneCount ===0) {
+            badge.style.display = 'none';
+        } else {
+            badge.style.display = 'flex';
+        }
+    }
+    updateBageV();
 });
